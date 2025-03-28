35106c822ce174945d9e62c221f0704bfae1165b
Dave Sommerville <dave.r.sommerville@outlook.com>
2025-03-28 10:51:44 -0500

    Files and structure created

---
+++ Havoc_And_Haven/Controllers/SuperheroController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Havoc_And_Haven.Controllers
+{
+    public class SuperheroController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Havoc_And_Haven/Controllers/VillainController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Havoc_And_Haven.Controllers
+{
+    public class VillainController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
