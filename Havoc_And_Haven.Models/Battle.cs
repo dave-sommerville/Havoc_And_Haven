4e220777fe77049f2dd9f4348303a359a739491a
Dave Sommerville <dave.r.sommerville@outlook.com>
2025-04-15 09:14:17 -0500

    Perusing and making notes

--- Havoc_And_Haven.Models/Battle.cs
+++ Havoc_And_Haven.Models/Battle.cs
@@ -4,7 +4,7 @@
     {
         public int BattleId { get; set; }
         public DateTime IncidentBegan { get; set; }
-        public string Winner { get; set; } // "Heroes" or "Villains"
+        public string? Winner { get; set; } // "Heroes" or "Villains"
         public int CrisisId { get; set; }
         public CrisisEvent CrisisEvent { get; set; }
 
