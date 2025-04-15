40c36119918e944687e04413ccb6c224e1d3fd28
Gur512 <gromana2022@gmail.com>
2025-04-15 15:58:39 -0500

    UpdateController

--- Havoc_And_Haven/Controllers/HeadquartersController.cs
+++ Havoc_And_Haven/Controllers/HeadquartersController.cs
@@ -8,18 +8,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Havoc_And_Haven.Controllers
 {
-    public class HeadquartersController : Controller
-    {
-<<<<<<< HEAD
-        //private readonly HeadquartersService _headquartersService;
-        //private readonly LocationService _locationService;
-
-        //public HeadquartersController(HeadquartersService headquartersService, LocationService locationService)
-        //{
-        //    _headquartersService = headquartersService;
-        //    _locationService = locationService;
-        //}
-=======
+    public class HeadquartersController : Controller {
         private readonly HeadquartersService _headquartersService;
         //private readonly LocationService _locationService;
 
@@ -28,66 +17,26 @@ namespace Havoc_And_Haven.Controllers
             _headquartersService = headquartersService;
             //_locationService = locationService;
         }
->>>>>>> b4db471dc0cb9bcb12d70caffc177efb82bc572b
-
-        //public IActionResult Index()
-        //{
-        //    List<Headquarters> headquarters = _headquartersService.GetAllHeadquarters();
-        //    return View(headquarters);
-        //}
-
-<<<<<<< HEAD
-        //[HttpGet]
-        //public IActionResult AddHeadquarter()
-        //{
-        //    ViewBag.Locations = _locationService.GetAllLocation();
-=======
+        public IActionResult Index() {
+            List<Headquarters> headquarters = _headquartersService.GetAllHeadquarters();
+            return View(headquarters);
+        }
         [HttpGet]
         public IActionResult Create()
         {
             //ViewBag.Locations = _locationService.GetAllLocation();
->>>>>>> b4db471dc0cb9bcb12d70caffc177efb82bc572b
-
-        //    return View(new Headquarters());
-        //}
-
-<<<<<<< HEAD
-        //[HttpPost]
-        //public IActionResult AddHeadquarter(Headquarters headquarter)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        headquarter.Location = _locationService.GetLocationById(headquarter.LocationId);
-=======
+            return View(new Headquarters());
+        }
         [HttpPost]
         public IActionResult Create(Headquarters headquarter)
         {
             if (ModelState.IsValid)
             {
                 //headquarter.Location = _locationService.GetLocationById(headquarter.LocationId);
->>>>>>> b4db471dc0cb9bcb12d70caffc177efb82bc572b
+                _headquartersService.AddHeadquarter(headquarter);
 
-        //        _headquartersService.AddHeadquarter(headquarter);
-
-        //        return RedirectToAction("Index");
-        //    }
-
-<<<<<<< HEAD
-        //    ViewBag.Locations = _locationService.GetAllLocation();
-        //    return View(headquarter);
-        //}
-
-        //[HttpGet]
-        //public IActionResult UpdateHeadquarters(int id)
-        //{
-        //    Headquarters? headquarter = _headquartersService.GetAllHeadquarters().FirstOrDefault(r => r.HeadquartersId == id);
-        //    if (headquarter == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    ViewBag.Locations = _locationService.GetAllLocation();
-=======
+                return RedirectToAction("Index");
+            }
             //ViewBag.Locations = _locationService.GetAllLocation();
             return View(headquarter);
         }
@@ -102,23 +51,8 @@ namespace Havoc_And_Haven.Controllers
             }
 
             //ViewBag.Locations = _locationService.GetAllLocation();
->>>>>>> b4db471dc0cb9bcb12d70caffc177efb82bc572b
-
-        //    return View(headquarter);
-        //}
-
-<<<<<<< HEAD
-        //[HttpPost]
-        //public IActionResult UpdateHeadquarters(Headquarters headquarter)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        _headquartersService.UpdateHeadquarters(headquarter);
-        //        return RedirectToAction("Index");
-        //    }
-
-        //    ViewBag.Locations = _locationService.GetAllLocation();
-=======
+            return View(headquarter);
+        }
         [HttpPost]
         public IActionResult Edit(Headquarters headquarter)
         {
@@ -129,35 +63,29 @@ namespace Havoc_And_Haven.Controllers
             }
 
             //ViewBag.Locations = _locationService.GetAllLocation();
->>>>>>> b4db471dc0cb9bcb12d70caffc177efb82bc572b
+            return View(headquarter);
+        }
 
-        //    return View(headquarter);
-        //}
+        [HttpGet]
+        public IActionResult DeleteHeadquarter(int id) {
+            Headquarters? headquarter = _headquartersService.GetHeadquarterById(id);
+            if (headquarter == null) {
+                return NotFound();
+            }
 
-        //[HttpGet]
-        //public IActionResult DeleteHeadquarter(int id)
-        //{
-        //    Headquarters? headquarter = _headquartersService.GetHeadquarterById(id);
-        //    if (headquarter == null)
-        //    {
-        //        return NotFound();
-        //    }
+            return View(headquarter);
+        }
 
-        //    return View(headquarter);
-        //}
+        [HttpPost]
+        public IActionResult DeleteHeadquarterConfirmed(int id) {
+            Headquarters? headquarter = _headquartersService.GetHeadquarterById(id);
+            if (headquarter == null) {
+                return NotFound();
+            }
 
-        //[HttpPost]
-        //public IActionResult DeleteHeadquarterConfirmed(int id)
-        //{
-        //    Headquarters? headquarter = _headquartersService.GetHeadquarterById(id);
-        //    if (headquarter == null)
-        //    {
-        //        return NotFound();
-        //    }
+            _headquartersService.DeleteHeadquarter(id);
 
-        //    _headquartersService.DeleteHeadquarter(id);
-
-        //    return RedirectToAction("Index");
-        //}
+            return RedirectToAction("Index");
+        }
     }
 }
