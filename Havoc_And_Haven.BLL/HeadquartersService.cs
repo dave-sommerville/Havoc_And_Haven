dc76dd527201fcd061393b9bc1c72648bf7a9115
Michael Shuaibu <shuaibumikel@gmail.com>
2025-04-14 20:57:09 -0500

    Headquarters setup done

---
+++ Havoc_And_Haven.BLL/HeadquartersService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Havoc_And_Haven.DAL;
+using Havoc_And_Haven.Models;
+
+namespace Havoc_And_Haven.BLL
+{
+    public class HeadquartersService
+    {
+        private readonly HeadquartersRepository _headquartersRepository;
+
+        public HeadquartersService(HeadquartersRepository headquartersRepository)
+        {
+            _headquartersRepository = headquartersRepository;
+        }
+
+        public List<Headquarters> GetAllHeadquarters()
+        {
+            return _headquartersRepository.GetAllHeadquarters();
+        }
+
+        public Headquarters GetHeadquarterById(int headquarterId)
+        {
+            return _headquartersRepository.GetHeadquarterById(headquarterId);
+        }
+
+        public void AddHeadquarter(Headquarters headquarter)
+        {
+            Console.WriteLine($"Registering headquarter: {headquarter.BaseTitle}");
+            _headquartersRepository.AddHeadquarter(headquarter);
+        }
+
+        public void UpdateHeadquarters(Headquarters headquarter)
+        {
+            _headquartersRepository.UpdateHeadquarters(headquarter);
+        }
+
+        public void DeleteHeadquarter(int HeadquartersId)
+        {
+            _headquartersRepository.DeleteHeadquarter(HeadquartersId);
+        }
+    }
+}
---
+++ Havoc_And_Haven.DAL/HeadquartersRepository.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Havoc_And_Haven.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Havoc_And_Haven.DAL
+{
+    public class HeadquartersRepository
+    {
+        private readonly HavocAndHavenDbContext _context;
+
+        public HeadquartersRepository(HavocAndHavenDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<Headquarters> GetAllHeadquarters() { 
+            return _context.Headquarters.ToList();
+        }
+
+        public Headquarters GetHeadquarterById(int id)
+        {
+            return _context.Headquarters
+                .Include(r => r.Location)
+                .FirstOrDefault(r => r.HeadquartersId == id);
+        }
+
+        public void AddHeadquarter(Headquarters headquarter)
+        {
+            try
+            {
+                _context.Headquarters.Add(headquarter);
+                _context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error saving racer: {ex.Message}");
+                throw;
+            }
+        }
+
+        public void UpdateHeadquarters(Headquarters headquarter)
+        {
+            Headquarters? existingHeadquarter = _context.Headquarters.Find(headquarter.HeadquartersId);
+            if (existingHeadquarter != null)
+            {
+                Console.WriteLine($"Updating Headquarter {headquarter.HeadquartersId} in database.");
+
+                existingHeadquarter.BaseTitle = headquarter.BaseTitle;
+                existingHeadquarter.Capacity = headquarter.Capacity;
+                existingHeadquarter.Description = headquarter.Description;
+
+                _context.Headquarters.Update(existingHeadquarter);
+                _context.SaveChanges();
+            }
+            else
+            {
+                Console.WriteLine("Headquarter not found for update.");
+            }
+        }
+
+        public void DeleteHeadquarter(int HeadquartersId)
+        {
+            Headquarters? headquarters = _context.Headquarters.Find(HeadquartersId);
+            if (headquarters != null)
+            {
+                _context.Headquarters.Remove(headquarters);
+                _context.SaveChanges(); 
+            }
+        }
+    }
+}
