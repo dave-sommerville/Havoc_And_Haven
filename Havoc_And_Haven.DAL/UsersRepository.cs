866254eeadb10577c471c974cd1543584786ae3a
Michael Shuaibu <shuaibumikel@gmail.com>
2025-04-16 21:56:37 -0500

    users fully functioning

--- Havoc_And_Haven.DAL/UsersRepository.cs
+++ Havoc_And_Haven.DAL/UsersRepository.cs
@@ -1,25 +1,75 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Havoc_And_Haven.Models;
+﻿using Havoc_And_Haven.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Havoc_And_Haven.DAL
 {
-    public  class UsersRepository
+    public class UserRepository
     {
         private readonly HavocAndHavenDbContext _context;
 
-        public UsersRepository(HavocAndHavenDbContext context)
+        public UserRepository(HavocAndHavenDbContext context)
         {
             _context = context;
-        }   
-        //public List<Users> GetAllUsers()
-        ////{
-        ////    return _context.Users
-        ////        .Include(u => u.Alias)
-        ////        .Include(u => u.Name);
-        ////}
+        }
+
+        public List<Users> GetAllUsers()
+        {
+            return _context.Users
+                .Include(u => u.Headquarters)
+                .Include(u => u.Lair)
+                .ToList();
+        }
+
+        public Users? GetUserById(int id)
+        {
+            return _context.Users
+                .Include(u => u.Headquarters)
+                .Include(u => u.Lair)
+                .FirstOrDefault(u => u.UserId == id);
+        }
+
+        public void AddUser(Users user)
+        {
+            try
+            {
+                _context.Users.Add(user);
+                _context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error saving user: {ex.Message}");
+                throw;
+            }
+        }
+
+        public void UpdateUser(Users user)
+        {
+            Users? existingUser = _context.Users.Find(user.UserId);
+            if (existingUser != null)
+            {
+                existingUser.FirstName = user.FirstName;
+                existingUser.LastName = user.LastName;
+                existingUser.Email = user.Email;
+                existingUser.Alias = user.Alias;
+                existingUser.OriginStory = user.OriginStory;
+                existingUser.Role = user.Role;
+                existingUser.PowerLevel = user.PowerLevel;
+                existingUser.HeadquartersId = user.HeadquartersId;
+                existingUser.LairId = user.LairId;
+
+                _context.Users.Update(existingUser);
+                _context.SaveChanges();
+            }
+        }
+
+        public void DeleteUser(int userId)
+        {
+            Users? user = _context.Users.Find(userId);
+            if (user != null)
+            {
+                _context.Users.Remove(user);
+                _context.SaveChanges();
+            }
+        }
     }
 }
---
+++ Havoc_And_Haven/Controllers/UsersController.cs
@@ -0,0 +1,125 @@
+﻿using Havoc_And_Haven.BLL;
+using Havoc_And_Haven.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Havoc_And_Haven.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly UserService _userService;
+        private readonly HeadquartersService _hqService;
+        private readonly LairService _lairService;
+
+        public UserController(UserService userService, HeadquartersService hqService, LairService lairService)
+        {
+            _userService = userService;
+            _hqService = hqService;
+            _lairService = lairService;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            List<Users> users = _userService.GetAllUsers();
+            return View(users);
+        }
+
+        [HttpGet]
+        public IActionResult AddUser()
+        {
+            List<Headquarters> headquarters = _hqService.GetAllHeadquarters();
+            List<Lair> lairs = _lairService.GetAllLairs();
+
+            ViewBag.Headquarters = headquarters;
+            ViewBag.Lairs = lairs;
+
+            return View(new Users());
+        }
+
+        [HttpPost]
+        public IActionResult AddUser(Users user)
+        {
+            if (ModelState.IsValid)
+            {
+                _userService.AddUser(user);
+                return RedirectToAction("Index");
+            }
+
+            List<Headquarters> headquarters = _hqService.GetAllHeadquarters();
+            List<Lair> lairs = _lairService.GetAllLairs();
+
+            ViewBag.Headquarters = headquarters;
+            ViewBag.Lairs = lairs;
+
+            return View(user);
+        }
+
+        [HttpGet]
+        public IActionResult EditUser(int id)
+        {
+            Users user = _userService.GetUserById(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            List<Headquarters> headquarters = _hqService.GetAllHeadquarters();
+            List<Lair> lairs = _lairService.GetAllLairs();
+
+            ViewBag.Headquarters = headquarters;
+            ViewBag.Lairs = lairs;
+
+            return View(user);
+        }
+
+        [HttpPost]
+        public IActionResult EditUser(Users user)
+        {
+            if (ModelState.IsValid)
+            {
+                _userService.UpdateUser(user);
+                return RedirectToAction("Index");
+            }
+
+            List<Headquarters> headquarters = _hqService.GetAllHeadquarters();
+            List<Lair> lairs = _lairService.GetAllLairs();
+
+            ViewBag.Headquarters = headquarters;
+            ViewBag.Lairs = lairs;
+
+            return View(user);
+        }
+
+        [HttpGet]
+        public IActionResult ViewProfile(int id)
+        {
+            Users user = _userService.GetUserById(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return View(user);
+        }
+
+        [HttpGet]
+        public IActionResult DeleteUser(int id)
+        {
+            Users user = _userService.GetUserById(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return View(user);
+        }
+
+        [HttpPost]
+        public IActionResult DeleteUserConfirmed(int id)
+        {
+            _userService.DeleteUser(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
